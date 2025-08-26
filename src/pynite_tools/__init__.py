ea382f7b775602e6be21bdaa21e75c81bc3caa42
Connor Ferster <connorferster@gmail.com>
2025-08-25 21:47:09 -0700

    bump version

--- src/pynite_tools/__init__.py
+++ src/pynite_tools/__init__.py
@@ -9,7 +9,7 @@ Modules included:
 - combos: Functions for bulk importing load combinations
 """
 
-__version__ = "0.6.1"
+__version__ = "0.6.2"
 
 from .reports import (
     extract_node_reactions,
